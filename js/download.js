f726bab97926241449dca81f295165d724cc7f25
Pritam Nanda <pritamnanda1998@gmail.com>
2023-11-27 15:34:29 +0530

    Create download.js

---
+++ js/download.js
@@ -0,0 +1,24 @@
+<script>
+  function downloadVideo() {
+    // Get the video element by its ID
+    var videoElement = document.getElementById('video');
+
+    // Get the video source element
+    var sourceElement = videoElement.querySelector('source');
+
+    // Get the video source URL
+    var videoUrl = sourceElement.src;
+
+    // Create a download link
+    var downloadLink = document.createElement("a");
+    downloadLink.href = videoUrl;
+    downloadLink.download = "downloaded-video"; // You can set a default filename here
+    document.body.appendChild(downloadLink);
+
+    // Trigger the download
+    downloadLink.click();
+
+    // Remove the download link from the document
+    document.body.removeChild(downloadLink);
+  }
+</script>
